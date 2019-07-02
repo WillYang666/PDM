4c2a620dd4e38d398badebe8ff14a6fcb4ff333e
WillYang666 <willyang9981@gmail.com>
2019-07-02 10:30:12 +0800

    treeview test update

--- PDM/MainForm.cs
+++ PDM/MainForm.cs
@@ -25,42 +25,58 @@ namespace PDM
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            string path = "/Users/will/Desktop/File";
-            string[] dirs = Directory.GetDirectories(path);
-            foreach (string item in dirs)
-            {
-                tv.Nodes.Add(Path.GetFileName(item));
-            }
-            string[] files = Directory.GetFiles(path);
-            foreach (string item in files)
-            {
-                tv.Nodes.Add(Path.GetFileName(item));
-            }
+            TreeNode tn = new TreeNode();
 
-            //调用该方法实现将指定路径下的子文件与子目录按照层次结构加载到TreeView
-            LoadFilesAndDirectoriesToTree(path, tv.Nodes);
+            tn.Text = "根目录";
+
+            getDirectories("", tn);
+
+            tv.Nodes.Add(tn);
         }
+           
 
-        private void LoadFilesAndDirectoriesToTree(string path, TreeNodeCollection treeNodeCollection)
+        private void getDirectories(string path, TreeNode tn)
         {
-            //1.先根据路径获取所有的子文件和子文件夹
-            string[] files = Directory.GetFiles(path);
-            string[] dirs = Directory.GetDirectories(path);
-            //2.把所有的子文件与子目录加到TreeView上。
-            foreach (string item in files)
-            {
-                //把每一个子文件加到TreeView上
-                treeNodeCollection.Add(Path.GetFileName(item));
-            }
-            //文件夹
-            foreach (string item in dirs)
-            {
-                TreeNode node = treeNodeCollection.Add(Path.GetFileName(item));
 
-                //由于目录，可能下面还存在子目录，所以这时要对每个目录再次进行获取子目录与子文件的操作
-                //这里进行了递归
-                LoadFilesAndDirectoriesToTree(item, node.Nodes);
+
+            string[] fileNames = Directory.GetFiles(path);
+            string[] directories = Directory.GetDirectories(path);
+
+
+            //先遍历这个目录下的文件夹
+            foreach (string dir in directories)
+            {
+
+                TreeNode subtn = new TreeNode();
+
+                subtn.Text = GetShorterFileName(dir);
+
+                getDirectories(dir, subtn);
+
+                tn.Nodes.Add(subtn);
+
+            }
+            //再遍历这个目录下的文件
+            foreach (string file in fileNames)
+            {
+
+                TreeNode subtn = new TreeNode();
+
+                subtn.Text = GetShorterFileName(file);
+
+                tn.Nodes.Add(subtn);
             }
         }
+        //去除路径
+        private string GetShorterFileName(string filename)
+        {
+
+            return filename.Substring(filename.LastIndexOf("\\") + 1);
+
+        }
+
+
+
+
     }
 }
